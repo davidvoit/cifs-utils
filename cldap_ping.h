f0450e9d374d3134a001c14c6ab7e86bcb8666f2
David Voit <david.voit@gmail.com>
2024-02-29 08:24:39 +0100

    Implement CLDAP Ping to find the closest site
    For domain based DFS we always need to contact the domain controllers.
    On setups, which are using bigger AD installations you could get random dc on the other side of the world,
    if you don't have site support. This can lead to network timeouts and other problems.
    
    CLDAP-Ping uses ASN.1 + UDP (CLDAP) and custom-DCE encoding including DName compressions without
    field separation. Finally after finding the sitename we now need to do a DNS SRV lookups to find
    the correct IPs to our closest site and fill up the remaining IPs from the global list.

---
+++ cldap_ping.h
@@ -0,0 +1,9 @@
+#ifndef _CLDAP_PING_H_
+#define _CLDAP_PING_H_
+
+// returns -1 of fatal errors, and -2 on network errors
+// if we get one of those retry do the cldap ping again on a another dc
+// site_name must be of MAXCDNAME size!
+int cldap_ping(char *domain, sa_family_t family, void *addr, char *site_name);
+
+#endif /* _CLDAP_PING_H_ */
